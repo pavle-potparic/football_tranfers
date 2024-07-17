b1281fbca2007922eb1ee99a5a6a74bf4ef4b8e3
pavle-potparic <pavle.potparic@gmail.com>
2024-07-17 16:25:10 +0200

    finished the navigation file

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import sqlite3
-from country_repository import get_all
+from country_repository import get_all, get_country, insert_country, delete_country, update_country
 
 main_menu = {1: 'Country', 2: 'Club', 3: 'Player', 0: 'Exit'}
 
 country_menu = {1: 'GET ALL COUNTRIES', 2: 'GET COUNTRY BY NAME', 3: 'INSERT COUNTRY',
                 4: 'UPDATE COUNTRY', 5: 'DELETE COUNTRY', 0: 'EXIT'}
 
-commands = []
-
 db = sqlite3.connect('football_transfers.sqlite')
 cursor = db.cursor()
-cursor.execute("INSERT INTO country VALUES (1, 'Srbija', 'Srb')")
-cursor.execute("INSERT INTO country VALUES (2, 'Francuska', 'Fra')")
-cursor.execute("INSERT INTO country VALUES (3, 'Hrvatska', 'Hrv')")
+cursor.execute("INSERT INTO country (name, code) VALUES ('Srbija', 'Srb')")
+cursor.execute("INSERT INTO country (name, code) VALUES ('Francuska', 'Fra')")
+cursor.execute("INSERT INTO country (name, code) VALUES ('Hrvatska', 'Hrv')")
 
 dict_level = 0
 
@@ -33,27 +31,37 @@ while True:
                 if question == 0:
                     break
 
-                elif question == 1 or question == 2:
+                elif question == 1:
                     print()
                     get_all(cursor)
                     print()
 
+                elif question == 2:
+                    print()
+                    name = input('Enter the country name: ')
+                    print()
+                    get_country(cursor, name)
+                    print()
+
                 elif question == 3:
-                    input_base = list(input('Enter the values: ').split(' '))
-                    commands = [country_menu[3], input_base]
+                    input_base = list(input('Enter the country name and the country code: ').split(' '))
+                    insert_country(cursor, input_base[0], input_base[1])
 
                 elif question == 4:
-                    input_base = list(input('Enter the values: ').split(' '))
-                    commands = [country_menu[4], input_base]
+                    name_or_code = input('Do you want to change name or code: ')
+                    input_base = list(input('Enter the original value for country and then new one: ').split(' '))
+                    update_country(cursor, name_or_code, input_base)
 
                 elif question == 5:
-                    input_base = input('Enter the value: ')
-                    commands = [country_menu[5], input_base]
+                    input_base = input('Enter the name of the country you want to delete: ')
+                    delete_country(cursor, input_base)
 
         elif question == 0:
             print()
-            print(commands)
             print('*****************************')
             print()
             print('CLOSING THE APP')
             break
+
+db.commit()
+db.close()
